266ead78099cbaae555d5daa3e888079432f3688
Timur Iskakov <info@champer.ru>
2022-01-02 23:18:40 +0300

    - Added basic classes for admin panel - Added Users.vue

---
+++ vue-admin/src/classes/user.ts
@@ -0,0 +1,19 @@
+import { Role } from '@/classes/role'
+
+export class User {
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  role: Role
+  permission: string[]
+
+  constructor (id = 0, first_name = '', last_name = '', role = new Role(), email = '', permission: string[] = []) {
+    this.id = id
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.role = role
+    this.permission = permission
+  }
+}
